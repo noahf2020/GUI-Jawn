095a660a1c83edb669889306725c4bd9b545459d
Noah Frank <64226459+noahf2020@users.noreply.github.com>
2023-11-21 14:58:16 -0500

    working on image scroll jawn

--- src/MyFxApp.java
+++ src/MyFxApp.java
@@ -2,27 +2,29 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.web.WebEngine;
+import javafx.scene.control.TextField;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
-import javafx.scene.control.ProgressBar;
-import javafx.scene.control.Label;
-import javafx.scene.control.MenuButton;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
-import javafx.scene.control.ColorPicker;
-import javafx.scene.paint.Color;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import java.io.FileInputStream;
-import javafx.scene.web.WebView;
+
 public class MyFxApp extends Application  {
-    double barStatus = 0.1;
+    int imageROll = 0;
+    int height = 100;
+    int width = 100;
+    String Meme;
+    String FontString = "arial";
     @Override
     public void start(Stage primaryStage)
 
@@ -31,76 +33,233 @@ public class MyFxApp extends Application  {
 
         primaryStage.setTitle("HBox Experiment 1");
 
-        Button button1 = new Button("Add Progress");
-        Button button2 = new Button("Subtract Progress");
+        Text texthe = new Text("Width");
+        TextField textFieldHeight = new TextField();
+        Text textwe = new Text("Height");
+        TextField textFieldWidth = new TextField();
 
-        Text text = new Text("This is a JavaFX text.");
-
-        ProgressBar progressBar = new ProgressBar(0);
-        Label label = new Label("My Label Jawn - Progress Bar");
+        Text textMeme = new Text("Enter Meme");
+        TextField textFieldMeme = new TextField();
 
 
-        progressBar.setProgress(barStatus);
 
-        button1.setOnAction(actionEvent -> {
-            barStatus = barStatus + 0.1;
-            progressBar.setProgress(barStatus);
+        Button button1 = new Button("Next Image ");
+        Button button2 = new Button("Previous Image");
+        Button button3 = new Button("Save Width And Height");
+        Button button4 = new Button("Save Meme");
+
+        ChoiceBox choiceBox = new ChoiceBox();
+        choiceBox.getItems().add("arial");
+        choiceBox.getItems().add("Comic sans MS");
+        choiceBox.getItems().add("Times New Roman");
+
+
+
+
+        Text text = new Text("To change image height and width, enter values like 200, default is 100");
+
+
+        ArrayList<Object> ImagesList = new ArrayList<>();
+
+        ImagesList.add("Images/WeUpBoys!!!!!!.png");
+        ImagesList.add("Images/graph.png");
+        ImagesList.add("Images/survey.png");
+        ImagesList.add("Images/setting.png");
+        ImagesList.add("Images/calendar.png");
+
+
+
+        choiceBox.setOnAction((event) -> {
+            int selectedIndex = choiceBox.getSelectionModel().getSelectedIndex();
+            Object selectedItem = choiceBox.getSelectionModel().getSelectedItem();
+
+            FontString = selectedItem.toString();
 
 
         });
+
+
+
+
+
+
+        button4.setOnAction(action -> {
+            Meme = textFieldMeme.getText();
+
+
+            Object imageFromList = ImagesList.get(imageROll);
+            String newImageString = imageFromList.toString();
+            FileInputStream input = null;
+            try {
+                input = new FileInputStream(newImageString);
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+            Image image = new Image(input);
+            ImageView imageView = new ImageView(image);
+
+
+            savenewWindow(imageView, height, width, button1,button2,button3,texthe,textFieldHeight,textwe,textFieldWidth,text,textMeme,textFieldMeme,primaryStage,button4,choiceBox);
+
+        });
+
+
+
+
+
+
+
+
+
+        button3.setOnAction(action -> {
+            String newHE = textFieldHeight.getText();
+            String newWI = textFieldWidth.getText();
+            height = Integer.parseInt(newWI);
+            width = Integer.parseInt(newHE);
+
+            Object imageFromList = ImagesList.get(imageROll);
+            String newImageString = imageFromList.toString();
+            FileInputStream input = null;
+            try {
+                input = new FileInputStream(newImageString);
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+            Image image = new Image(input);
+            ImageView imageView = new ImageView(image);
+
+
+            savenewWindow(imageView, height, width, button1,button2,button3,texthe,textFieldHeight,textwe,textFieldWidth,text,textMeme,textFieldMeme,primaryStage,button4,choiceBox);
+
+        });
+
 
         button2.setOnAction(actionEvent -> {
-            barStatus = barStatus - 0.1;
-            progressBar.setProgress(barStatus);
-        });
+            imageROll = imageROll - 1;
+            FileInputStream input = null;
+            try {
+                Object imageFromList = ImagesList.get(imageROll);
 
-        MenuItem menuItem1 = new MenuItem("Jawn 1");
-        MenuItem menuItem2 = new MenuItem("Jawn 2");
-        MenuItem menuItem3 = new MenuItem("Jawn 3");
+                String newImageString = imageFromList.toString();
+                input = new FileInputStream(newImageString);
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            }
 
-        MenuButton menuButton = new MenuButton("Pick a jawn", null, menuItem1, menuItem2, menuItem3);
+            Image image = new Image(input);
+            ImageView imageView = new ImageView(image);
 
+            savenewWindow(imageView, height, width, button1,button2,button3,texthe,textFieldHeight,textwe,textFieldWidth,text,textMeme,textFieldMeme,primaryStage,button4,choiceBox);
 
-        CheckBox checkBox1 = new CheckBox("Green");
-        RadioButton radioButton1 = new RadioButton("Left");
-        //    TextArea textArea = new TextArea();
-       // TextField textField = new TextField();
-        Text text2 = new Text("Change Progress Bar Color: ");
-        Button button3 = new Button("Confirm Color Change");
-
-        ColorPicker colorPicker = new ColorPicker();
-
-
-        button3.setOnAction(actionEvent -> {
-            Color value = colorPicker.getValue();
-            String colorValue = value.toString();
-            String newColor = colorValue.substring(2);
-            progressBar.setStyle("-fx-accent:" + "#" +newColor);
         });
 
 
-        FileInputStream input = new FileInputStream("Images/WeUpBoys!!!!!!.png");
+        button1.setOnAction(actionEvent -> {
+            imageROll = imageROll +1;
+            FileInputStream input = null;
+            try {
+                Object imageFromList = ImagesList.get(imageROll);
+                String newImageString = imageFromList.toString();
+                input = new FileInputStream(newImageString);
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+
+            Image image = new Image(input);
+            ImageView imageView = new ImageView(image);
+
+            savenewWindow(imageView, height, width, button1,button2,button3,texthe,textFieldHeight,textwe,textFieldWidth,text,textMeme,textFieldMeme,primaryStage,button4,choiceBox);
+
+
+        });
+
+
+
+        Object imageFromList = ImagesList.get(imageROll);
+        String newImageString = imageFromList.toString();
+        FileInputStream input = new FileInputStream(newImageString);
         Image image = new Image(input);
         ImageView imageView = new ImageView(image);
-        imageView.setFitHeight(200);
-        imageView.setFitWidth(350);
 
 
-        HBox hbox = new HBox(button1, button2);
-        VBox h2box = new VBox(radioButton1,text2,colorPicker,button3,checkBox1);
+        savenewWindow(imageView, height, width, button1,button2,button3,texthe,textFieldHeight,textwe,textFieldWidth,text,textMeme,textFieldMeme,primaryStage,button4,choiceBox);
 
-        VBox Vbox1 = new VBox(text,label, progressBar,hbox,h2box,imageView);
-
-
-        Scene scene = new Scene(Vbox1, 500, 400);
-
-
-
-
-        primaryStage.setScene(scene);
-
-        primaryStage.show();
     }
+
+
+    public void  savenewWindow
+            (ImageView imageView,
+             int height,
+             int width,
+             Button button1,
+             Button button2,
+             Button button3,
+             Text texthe,
+             TextField textFieldHeight,
+             Text textwe,
+             TextField textFieldWidth,
+             Text text,
+             Text textMeme,
+             TextField textFieldMeme,
+             Stage primaryStage,
+             Button button4,
+             ChoiceBox choiceBox
+            )  {
+
+        imageView.setFitHeight(height);
+        imageView.setFitWidth(width);
+
+       System.out.println(Meme);
+        Label label = new Label(Meme);
+
+        label.setFont(Font.font (FontString, 20));
+
+        HBox hbox = new HBox(button1, button2,button3);
+        HBox hbox1 = new HBox(texthe, textFieldHeight);
+        HBox hbox2 = new HBox(textwe, textFieldWidth);
+        HBox hbox3 = new HBox(textMeme, textFieldMeme, button4,choiceBox);
+
+        Text ImagerollJawn = new Text("Image: "+imageROll +"/4");
+
+        StackPane stack_pane = new StackPane(imageView, label);
+
+
+        try {
+
+        FileInputStream input = new FileInputStream("Images/graph.png");
+        Image image = new Image(input);
+
+        FileInputStream input2 = new FileInputStream("Images/survey.png");
+        Image image2 = new Image(input2);
+
+        ScrollPane scrollPane = new ScrollPane();
+         ImageView mainImageView = new ImageView(image);
+         ImageView bgImageView = new ImageView(image2);
+            mainImageView.setFitHeight(100);
+            mainImageView.setFitWidth(300);
+            bgImageView.setFitHeight(200);
+            bgImageView.setFitWidth(300);
+
+
+
+            FlowPane imagePane = new FlowPane();
+            imagePane.getChildren().addAll(bgImageView, mainImageView);
+            scrollPane.setContent(imagePane);
+            scrollPane.setPrefSize(400, 400);
+
+
+        HBox hboxImageScroll = new HBox(scrollPane);
+        VBox Vbox1 = new VBox(text, ImagerollJawn, hbox, hbox1, hbox2,hbox3, stack_pane,hboxImageScroll);
+        Scene scene = new Scene(Vbox1, 550, 450);
+        scene.getStylesheets().add("Style.css");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
 
     public static void main(String[] args) {
         Application.launch(args);
